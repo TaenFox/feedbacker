dbeeebb674437c6f8fb16a8b88d73db51e05519a
Павел Мокеев <mokeev.pavel@gmail.com>
2024-09-22 11:22:23 +0300

    Interface addFeedbackById has been added

--- _test.py
+++ _test.py
@@ -4,10 +4,11 @@ import uuid, json
 
 d = db.FileDataBase("feedback")
 i = str(uuid.uuid4())
-d.addById(i, {"1":1})
-
-d.modifyById(i, {"1":2})
-
-i = "36d1395b-c0a5-4a11-a0d1-eddcf1408862"
+print(f"./data/feedback/{i}.json")
+data = {
+    "1":1,
+    "2":2
+}
+dbi.addFeedbackById(i, data)
 
 print(json.dumps(dbi.getFeedbackById(i)))
--- data/database_interface.py
+++ data/database_interface.py
@@ -10,5 +10,17 @@ def getFeedbackById(feedbackId:str)-> dict:
         print(f"Can't get feedback by id = {feedbackId}: {e}")
         return None
 
-def modifyFeedbackById(feedback:str, data:dict)-> dict:
+def addFeedbackById(feedbackId:str, data:dict)-> dict:
+    '''Сохраняет словарь значений, переданных в словаре data. 
+    Сохраняется с id={feedbackId}. Возвращает результат сохранения'''
+    try:
+        db_feedback = db_interface("feedback")
+        db_feedback.addById(feedbackId, data)
+        data = db_feedback.getById(feedbackId)
+        return data
+    except Exception as e:
+        print(f"Can't add feedback by id = {feedbackId}: {e}")
+        return None
+
+def modifyFeedbackById(feedbackId:str, data:dict)-> dict:
     pass
