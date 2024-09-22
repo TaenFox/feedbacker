6294eee8bca1bd41f15c5eea08e52377c56fa35b
Павел Мокеев <mokeev.pavel@gmail.com>
2024-09-22 08:18:16 +0300

    feedback has been added

---
+++ model/feedback.py
@@ -0,0 +1,35 @@
+
+class feedback():
+    feedbackId: str
+    content: str
+    authorId: str
+    receiverId: str
+
+    def __init__(self, data: dict) -> None:
+        '''
+        Объект фидбека. В аргументы передаётся словарь с ключами (значения в них строковые):
+        - feedbackId - идентификатор фидбека
+        - content - сообщение фидбека
+        - authorId - идентификатор автора фидбека
+        - receiverId - идентификатор получателя фидбека
+        '''
+        try:
+            if "feedbackId" not in data: raise Exception("No 'feedbackId' in 'data'")
+            if "content" not in data: raise Exception("No 'content' in 'data'")
+            if "authorId" not in data: raise Exception("No 'authorId' in 'data'")
+            if "receiverId" not in data: raise Exception("No 'receiverId' in 'data'")
+        except Exception as e:
+            print(e)
+            return None
+        
+        try:
+            self.feedbackId = data['feedbackId']
+            self.content = data['content']
+            self.authorId = data['authorId']
+            self.receiverId = data['receiverId']
+        except Exception as e:
+            print(f"Can't use 'data' dictionary: {e}")
+
+        
+    def new(self, data)-> None:
+        pass
