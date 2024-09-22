455a971a035f7d63e4710817f593699a8b3cfda4
Павел Мокеев <mokeev.pavel@gmail.com>
2024-09-22 11:01:26 +0300

    Interface getFeedbackById has been added

--- _test.py
+++ _test.py
@@ -1,8 +1,11 @@
 import data.file_database as db
-import uuid
+import data.database_interface as dbi
+import uuid, json
 
 d = db.FileDataBase("feedback")
 i = str(uuid.uuid4())
 d.addById(i, {"1":1})
 
 d.modifyById(i, {"1":2})
+
+print(json.dumps(dbi.getFeedbackById(i)))
--- data/database_interface.py
+++ data/database_interface.py
@@ -1,8 +1,15 @@
-import file_database
+from data.file_database import FileDataBase as db_interface
 
 def getFeedbackById(feedbackId:str)-> dict:
     '''Получает словарь значений, соответствующих фидбеку с id={feedbackId}'''
-    pass
+    try:
+        db_feedback = db_interface("feedback")
+        data = db_feedback.getById(feedbackId)
+        if data == None: raise Exception("No data has been read")
+        return data
+    except Exception as e:
+        print(f"Can't get feedback by id = {feedbackId}: {e}")
+        return None
 
 def modifyFeedbackById(feedback:str, data:dict)-> dict:
     pass
