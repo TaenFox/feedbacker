a4ca75df9ad66734d7e56944a5c1c4f20ffb29ca
Павел Мокеев <mokeev.pavel@gmail.com>
2024-09-22 10:33:49 +0300

    Class FileDataBase has been created

--- _test.py
+++ _test.py
@@ -1,4 +1,5 @@
 import data.file_database as db
 import uuid
 
-db.addById(str(uuid.uuid4()), {"1":1})
+d = db.FileDataBase("feedback")
+d.addById(str(uuid.uuid4()), {"1":1})
--- data/file_database.py
+++ data/file_database.py
@@ -1,32 +1,36 @@
-import json
+import json, os
 
-def getById(id:str)-> dict:
-    '''Извлекает информацию из json-файла в текущей директории, 
-    название которого соответствует маске `{id}.json`. 
-    Возвращает словарь'''
-    try:
-        with open(f"{id}.json", "r", encoding="utf-8") as json_file:
-            data = json.load(json_file)
-            return data
-    except Exception as e:
-        print(f"Can't read file {id}.json: {e}")
+class FileDataBase():
+    def __init__(self, catalog:str) -> None:
+        self.catalog = f"{os.curdir}/data/{catalog}"
 
-def addById(id:str, data:dict)-> dict:
-    '''Сохраняет информацию в json-файл в текущей директории, 
-    название которого соответствует маске `{id}.json`.
-    В случае успеха возвращает сохранённый словарь значений'''
-    try:
-        json_file = open(f"{id}.json", "x", encoding="utf-8")
-        json_file.write(json.dumps(data))
-        json_file.close()
-    except Exception as e:
-        print(f"Can't create file {id}.json: {e}")
-        return None
-    
-    try:
-        savedData = getById(id=id)
-        if data != savedData: raise Exception("Input data doesn't match to saved data")
-        return savedData
-    except Exception as e:
-        print(f"Error creating file {id}.json: {e}")
-        return None
+    def getById(self, id:str)-> dict:
+        '''Извлекает информацию из json-файла в текущей директории, 
+        название которого соответствует маске `{id}.json`. 
+        Возвращает словарь'''
+        try:
+            with open(f"{self.catalog}/{id}.json", "r", encoding="utf-8") as json_file:
+                data = json.load(json_file)
+                return data
+        except Exception as e:
+            print(f"Can't read file {id}.json: {e}")
+
+    def addById(self, id:str, data:dict)-> dict:
+        '''Сохраняет информацию в json-файл в текущей директории, 
+        название которого соответствует маске `{id}.json`.
+        В случае успеха возвращает сохранённый словарь значений'''
+        try:
+            json_file = open(f"{self.catalog}/{id}.json", "x", encoding="utf-8")
+            json_file.write(json.dumps(data))
+            json_file.close()
+        except Exception as e:
+            print(f"Can't create file {id}.json: {e}")
+            return None
+        
+        try:
+            savedData = self.getById(id=id)
+            if data != savedData: raise Exception("Input data doesn't match to saved data")
+            return savedData
+        except Exception as e:
+            print(f"Error creating file {id}.json: {e}")
+            return None
