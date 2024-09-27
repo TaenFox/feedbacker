1c59e7abdca742be42f54f408a352f8b3e4764d0
Павел Мокеев <mokeev.pavel@gmail.com>
2024-09-27 13:39:47 +0300

    Test "test_wrong_type_feedback_id" has been added

--- model/feedback_model_test.py
+++ model/feedback_model_test.py
@@ -17,3 +17,11 @@ def test_create_feedback_model(fix_feedback_data):
     - создание экземпляра класса
     - выдачу данных экземпляра класса как словарь'''
     assert Feedback(fix_feedback_data).to_dict() == fix_feedback_data
+
+def test_wrong_type_feedback_id(fix_feedback_data):
+    '''Тест проверяет корректность отработки исключения 
+    на строковый тип данных в поле feedback_id'''
+    wrong_data = fix_feedback_data
+    wrong_data["feedback_id"] = uuid.uuid4()
+    with pytest.raises(Exception):
+        Feedback(wrong_data)
