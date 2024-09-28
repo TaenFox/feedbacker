123ec119ae045fb977b0e40d075a33b0fadd0849
Павел Мокеев <mokeev.pavel@gmail.com>
2024-09-28 19:48:32 +0300

    correction uc

--- test/usecase_test.py
+++ test/usecase_test.py
@@ -13,19 +13,19 @@ def temp_catalog_feedback(tmp_path):
     catalog.mkdir(parents=True, exist_ok=True)
     return catalog
 
-def test_send_feedback(temp_catalog_feedback):
+def test_data_sent_with_feedback(temp_catalog_feedback):
     feedback_id = str(uuid.uuid4())
     author_id = str(uuid.uuid4())
-    reciever_id = str(uuid.uuid4())
+    receiver_id = str(uuid.uuid4())
     feedback_content = "test context string"
     reference_data = {
         "feedback_id": feedback_id,
         "content": feedback_content,
         "author_id": author_id,
-        "reciever_id": reciever_id
+        "receiver_id": receiver_id
     }
-    recieved_data = uc.save_feedback(author_id, reciever_id, feedback_content, feedback_id)
+    recieved_data = uc.save_feedback(author_id, receiver_id, feedback_content, feedback_id, temp_catalog_feedback)
 
-    saved_data = FeedbackDTO().get_feedback_by_id(feedback_id)
+    saved_data = FeedbackDTO(temp_catalog_feedback).get_feedback_by_id(feedback_id)
 
     assert reference_data == saved_data
--- usecases/send_feedback.py
+++ usecases/send_feedback.py
@@ -8,20 +8,24 @@ from data.database_interface import FeedbackDTO, UserDTO
 import uuid
 
 def save_feedback(author_id:str,
-            reciever_id:str,
+            receiver_id:str,
             feedback_context:str,
             feedback_id:str = "",
             test_path = ""):
     if feedback_id == "": feedback_id = str(uuid.uuid4())
+    feedback_obj = Feedback()
+
     feedback_data = {
         "feedback_id": feedback_id,
         "content": feedback_context,
         "author_id": author_id,
-        "reciever_id": reciever_id
+        "receiver_id": receiver_id
     }
     
     path = "" #стандартный путь к хранилищу
     if test_path != "": path = test_path
 
-    saved_data = FeedbackDTO(path).add_feedback_by_id(feedback_id, feedback_data)
+    feedback_obj.from_dict(feedback_data)
+
+    saved_data = FeedbackDTO(path).add_feedback_by_id(feedback_id, feedback_obj.to_dict())
     return saved_data
